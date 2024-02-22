07c4c09866ef22bbc4ba686a5fe2a66afb89058a
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2024-02-22 19:40:02 +0200

    add documentation

--- 02-MCAL/01-RCC/RCC_interface.h
+++ 02-MCAL/01-RCC/RCC_interface.h
@@ -10,8 +10,24 @@
 #define RCC_APB1    1
 #define RCC_APB2    2
 
+/**
+ * @brief initialize system clock based on configurations
+ * 
+ */
 void MRCC_voidInitSysClock(void);
+/**
+ * @brief Enable clock for a specific periphral
+ * 
+ * @param copy_u8BusId the bus that the periphral is connected to
+ * @param copy_u8PerId the periphral ID number 
+ */
 void MRCC_voidEnableClock(u8 copy_u8BusId, u8 copy_u8PerId);
+/**
+ * @brief Disable clock for a specific periphral
+ * 
+ * @param copy_u8BusId the bus that the periphral is connected to
+ * @param copy_u8PerId the periphral ID number 
+ */
 void MRCC_voidDisabeClock(u8 copy_u8BusId, u8 copy_u8PerId);
 
 #endif
