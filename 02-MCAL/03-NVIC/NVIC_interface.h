8ec619468e5268b359670a6ac618c01a1cc57a70
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2024-06-27 20:02:43 +0300

    Change names of functions for naming convention

--- 02-MCAL/03-NVIC/NVIC_interface.h
+++ 02-MCAL/03-NVIC/NVIC_interface.h
@@ -6,10 +6,10 @@
 #ifndef NVIC_INTERFACE_H
 #define NVIC_INTERFACE_H
 
-void MNVIC_VoidEnableInterrupt(u8 Copy_u8IntNumber);
-void MNVIC_VoidDisableInterrupt(u8 Copy_u8IntNumber);
-void MNVIC_VoidSetPendingFlag(u8 Copy_u8IntNumber);
-void MNVIC_VoidDisablePendingFlag(u8 Copy_u8IntNumber);
+void MNVIC_voidEnableInterrupt(u8 Copy_u8IntNumber);
+void MNVIC_voidDisableInterrupt(u8 Copy_u8IntNumber);
+void MNVIC_voidSetPendingFlag(u8 Copy_u8IntNumber);
+void MNVIC_voidDisablePendingFlag(u8 Copy_u8IntNumber);
 u8 MNVIC_u8GetActiveFlag(u8 Copy_u8IntNumber);
 void MNVIC_voidSetPriority(s8 Copy_s8IntID , u8 Copy_u8GroupPriority ,u8 Copy_u8SubPriority );
 
